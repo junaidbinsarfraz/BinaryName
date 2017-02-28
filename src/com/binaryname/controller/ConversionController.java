a313d256bb9d4f961961977ed11a7efb97901dbb
AM GIMINT (Farhan Hamayun) <farhanhamayun@INT.GOV.PK>
2017-02-28 12:55:48 +0500

    Java docs added

--- src/com/binaryname/controller/ConversionController.java
+++ src/com/binaryname/controller/ConversionController.java
@@ -1,22 +1,32 @@
 package com.binaryname.controller;
 
+/**
+ * The ConversionController is use to convert string to binary form
+ */
 public class ConversionController {
-	
+
+	/**
+	 * The getBinaryName is use to convert string to binary form
+	 * 
+	 * @param name
+	 *            to be converted
+	 * @return converted binary name
+	 */
 	public String getBinaryName(String name) {
-		
-		if(name == null || name.isEmpty()) {
+
+		if (name == null || name.isEmpty()) {
 			return "";
 		}
-		
-		String result = "";
-	    char[] messChar = name.toCharArray();
 
-	    for (int i = 0; i < messChar.length; i++) {
-	        result += Integer.toBinaryString(messChar[i]) + " ";
-	    }
-	    
-	    return result;
-		
+		String result = "";
+		char[] messChar = name.toCharArray();
+
+		for (int i = 0; i < messChar.length; i++) {
+			result += Integer.toBinaryString(messChar[i]) + " ";
+		}
+
+		return result;
+
 	}
-	
+
 }
--- src/com/binaryname/view/Main.java
+++ src/com/binaryname/view/Main.java
@@ -8,7 +8,6 @@ import com.sun.javafx.print.Units;
 import com.sun.javafx.scene.control.skin.FXVK;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -41,6 +40,9 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.Window;
 
+/**
+ * The Main class is use to show Javafx gui and virtual keyboard for user interaction
+ */
 public class Main extends Application {
 
 	private ConversionController conversionController = new ConversionController();
@@ -61,19 +63,23 @@ public class Main extends Application {
 		helloLbl.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 68));
 		helloLbl.setStyle("-fx-background-color: red;padding: 20px;");
 		helloLbl.setTextFill(Color.web("#ffffff"));
-		
+
 		Label myNameLbl = new Label("my name is");
 		myNameLbl.setAlignment(Pos.CENTER);
 		myNameLbl.setFont(Font.font("Comic Sans MS", 48));
 		myNameLbl.setStyle("-fx-background-color: red;padding: 20px;");
 		myNameLbl.setTextFill(Color.web("#ffffff"));
-		
+
 		TextArea nameTxtArea = new TextArea();
 		nameTxtArea.setWrapText(Boolean.TRUE);
 		nameTxtArea.getStyleClass().add("center-text-area");
 		nameTxtArea.setFont(Font.font("Comic Sans MS", 28));
 		nameTxtArea.setStyle("padding: 20px;");
-		
+
+		Label dummy = new Label(" ");
+		dummy.setStyle("-fx-background-color: red;");
+		dummy.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 10));
+
 		Button printBtn = new Button("PRINT");
 		printBtn.setId("ipad-grey");
 		printBtn.setDisable(Boolean.TRUE);
@@ -84,10 +90,10 @@ public class Main extends Application {
 
 			@Override
 			public void handle(ActionEvent event) {
-				nameTxtArea.requestFocus();
 				nameTxtArea.setText(conversionController.getBinaryName(nameTxtArea.getText()));
 				convertBtn.setDisable(Boolean.TRUE);
 				printBtn.setDisable(Boolean.FALSE);
+				nameTxtArea.requestFocus();
 			}
 		});
 
@@ -97,19 +103,19 @@ public class Main extends Application {
 
 		VBox vBox = new VBox(10);
 		vBox.setAlignment(Pos.TOP_CENTER);
-		vBox.getChildren().addAll(helloLbl, myNameLbl, nameTxtArea, hBox);
+		vBox.getChildren().addAll(helloLbl, myNameLbl, nameTxtArea, dummy, hBox);
 		vBox.setStyle("-fx-background-color: red;margin: 20px;");
 
 		printBtn.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				nameTxtArea.requestFocus();
 				// Start printing
 				print(vBox, nameTxtArea.getText());
 				convertBtn.setDisable(Boolean.FALSE);
 				printBtn.setDisable(Boolean.TRUE);
 				nameTxtArea.setText("");
+				nameTxtArea.requestFocus();
 			}
 		});
 
@@ -123,11 +129,11 @@ public class Main extends Application {
 		primaryStage.setY(visualBounds.getMinY());
 		primaryStage.setWidth(visualBounds.getWidth());
 		primaryStage.setHeight(visualBounds.getHeight());
-		
+
 		adjustTextAreaLayout(nameTxtArea);
-		
+
 		primaryStage.show();
-		
+
 		// attach keyboard to first node on scene:
 		Node first = scene.getRoot().getChildrenUnmodifiable().get(0);
 		if (first != null) {
@@ -136,107 +142,63 @@ public class Main extends Application {
 			keyboard = getPopupWindow();
 		}
 		
+		// Attach the on focus listener
 		nameTxtArea.focusedProperty().addListener((ob, b, b1) -> {
 			if (keyboard == null) {
 				keyboard = getPopupWindow();
 			}
-			
+
 			keyboard.setHideOnEscape(Boolean.FALSE);
 			keyboard.setAutoHide(Boolean.FALSE);
 			keyboard.centerOnScreen();
 			keyboard.requestFocus();
-			
-			keyboard.yProperty().addListener(obs -> {
-				
+
+			Double y = bounds.getHeight() - taskbarHeight - keyboard.getY();
+			nameTxtArea.setMaxHeight((bounds.getHeight() - y));
+			nameTxtArea.setMinHeight((bounds.getHeight() - y));
+			nameTxtArea.setPrefHeight((bounds.getHeight() - y));
+
+			/*keyboard.yProperty().addListener(obs -> {
+
 				Platform.runLater(() -> {
-					Double y = bounds.getHeight() - taskbarHeight - keyboard.getY();
-					nameTxtArea.setMaxHeight((bounds.getHeight() - y) * 0.4);
-					nameTxtArea.setMinHeight((bounds.getHeight() - y) * 0.4);
 				});
-			});
-			
-			/*keyboard.setOnAutoHide(new EventHandler<Event>() {
-				
-				@Override
-				public void handle(Event event) {
-					System.out.println("I am here");
-				}
-				
-			});
-			
-			keyboard.setOnCloseRequest(new EventHandler<WindowEvent>() {
-				
-				@Override
-				public void handle(WindowEvent event) {
-					System.out.println("I am here");
-				}
-				
 			});*/
+
 		});
-		
-
-//						primaryStage.setY(y > 0 ? -y : 0);
-		/*nameTxtArea.onMouseClickedProperty().addListener((ob, b, b1) -> {
-			if (keyboard == null) {
-				keyboard = getPopupWindow();
-
-				keyboard.yProperty().addListener(obs -> {
-					System.out.println("wi " + keyboard.getY());
-					Platform.runLater(() -> {
-						double y = bounds.getHeight() - taskbarHeight - keyboard.getY();
-						primaryStage.setY(y > 0 ? -y : 0);
-					});
-				});
-			}
-		});*/
-		
-		/*nameTxtArea.setOnMouseClicked(new EventHandler<MouseEvent>() {
-
-	        @Override
-	        public void handle(MouseEvent event) {
-	        	if (keyboard == null) {
-					keyboard = getPopupWindow();
-
-					keyboard.yProperty().addListener(obs -> {
-						System.out.println("wi " + keyboard.getY());
-						Platform.runLater(() -> {
-							double y = bounds.getHeight() - taskbarHeight - keyboard.getY();
-							primaryStage.setY(y > 0 ? -y : 0);
-						});
-					});
-				}
-	        	
-	        	keyboard.show(primaryStage);
-	        }
-	    });*/
-		
-//		keyboard = getPopupWindow();
-		
-		/*keyboard.onCloseRequestProperty().addListener((ob, b, b1) -> {
-			System.out.println("I am here");
-		});*/
 
 	}
-
+	
+	/**
+	 * The main() method is use to launch the Javafx gui.
+	 * @param args not used
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	/**
+	 * The print() method is use to print the text with given dimension
+	 * 
+	 * @param node1
+	 *            to be printed
+	 * @param text
+	 *            to be written
+	 */
 	private void print(Node node1, String text) {
 		// Create a printer job for the default printer
-		
+
 		Printer printer = Printer.getDefaultPrinter();
 		Paper label = PrintHelper.createPaper("2.5x3.5", 2.5, 3.5, Units.INCH);
 		PageLayout pageLayout = printer.createPageLayout(label, PageOrientation.LANDSCAPE, Printer.MarginType.EQUAL);
 
 		PrinterJob job = PrinterJob.createPrinterJob();
-		
+
 		Node node = createFullNode(text);
-		
+
 		double scaleX = pageLayout.getPrintableWidth() / node1.getBoundsInParent().getWidth();
 		double scaleY = pageLayout.getPrintableHeight() / node1.getBoundsInParent().getHeight();
 		node.getTransforms().add(new Scale(scaleX, scaleY));
-		
+
 		if (job != null) {
 			// Print the node
 			boolean printed = job.printPage(node);
@@ -252,11 +214,16 @@ public class Main extends Application {
 			// Write Error Message
 			System.out.println("Could not create a printer job.");
 		}
-		
+
 		node.getTransforms().remove(node.getTransforms().size() - 1);
 	}
 
-	/*private PopupWindow getPopupWindow() {
+	/**
+	 * The getPopupWindow() method is use to show keyboard on demand
+	 * 
+	 * @return the keyboard instance
+	 */
+	private PopupWindow getPopupWindow() {
 
 		@SuppressWarnings("deprecation")
 		final Iterator<Window> windows = Window.impl_getWindows();
@@ -270,8 +237,8 @@ public class Main extends Application {
 						Node popup = root.getChildrenUnmodifiable().get(0);
 						if (popup.lookup(".fxvk") != null) {
 							FXVK vk = (FXVK) popup.lookup(".fxvk");
-                            // hide the key:
-                            vk.lookup(".hide").setVisible(false);
+							// hide the keyboard-hide key
+							vk.lookup(".hide").setVisible(false);
 							return (PopupWindow) window;
 						}
 					}
@@ -280,36 +247,18 @@ public class Main extends Application {
 			}
 		}
 		return null;
-	}*/
-	
-	private PopupWindow getPopupWindow() {
+	}
 
-        @SuppressWarnings("deprecation") 
-        final Iterator<Window> windows = Window.impl_getWindows();
-
-        while (windows.hasNext()) {
-            final Window window = windows.next();
-            if (window instanceof PopupWindow) {
-                if (window.getScene() != null && window.getScene().getRoot() != null) { 
-                    Parent root = window.getScene().getRoot();
-                    if (root.getChildrenUnmodifiable().size() > 0) {
-                        Node popup = root.getChildrenUnmodifiable().get(0);
-                        if (popup.lookup(".fxvk") != null) {
-                            FXVK vk = (FXVK) popup.lookup(".fxvk");
-                            // hide the key:
-                            vk.lookup(".hide").setVisible(false);
-                            return (PopupWindow) window;
-                        }
-                    }
-                }
-                return null;
-            }
-        }
-        return null;
-    }
-	
+	/**
+	 * The createFullNode() method is use to create an imaginary node that will
+	 * be used for printing
+	 * 
+	 * @param text
+	 *            that will be printed
+	 * @return the node to be printed
+	 */
 	private Node createFullNode(String text) {
-		
+
 		Label helloLbl = new Label("Hello");
 		helloLbl.setAlignment(Pos.CENTER);
 		helloLbl.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 68));
@@ -329,44 +278,56 @@ public class Main extends Application {
 		nameTxtArea.setText(text);
 		nameTxtArea.getStyleClass().add("center-text-area");
 
-		HBox hBox = new HBox(1000);
-		hBox.setAlignment(Pos.CENTER);
+		Label dummy = new Label(" ");
+		dummy.setStyle("-fx-background-color: red;");
+		dummy.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 30));
 
 		VBox vBox = new VBox(10);
 		vBox.setAlignment(Pos.CENTER);
-		vBox.getChildren().addAll(helloLbl, myNameLbl, nameTxtArea, hBox);
+		vBox.getChildren().addAll(helloLbl, myNameLbl, nameTxtArea, dummy);
 		vBox.setStyle("-fx-background-color: red;margin: 20px;");
-		
+
 		vBox.getStylesheets().add(Main.class.getResource("/style.css").toExternalForm());
-		
+
 		return vBox;
 	}
-	
+
+	/**
+	 * The adjustTextAreaLayout() method is use to adjust the textarea layout.
+	 * It will attach the listener when text is changed
+	 * 
+	 * @param textArea
+	 */
 	private void adjustTextAreaLayout(TextArea textArea) {
-        textArea.applyCss();
-        textArea.layout();
+		textArea.applyCss();
+		textArea.layout();
 
-        ScrollPane textAreaScroller = (ScrollPane) textArea.lookup(".scroll-pane");
-        Text text = (Text) textArea.lookup(".text");
+		ScrollPane textAreaScroller = (ScrollPane) textArea.lookup(".scroll-pane");
+		Text text = (Text) textArea.lookup(".text");
 
+		ChangeListener<? super Bounds> listener = (obs, oldBounds, newBounds) -> centerTextIfNecessary(textAreaScroller, text);
+		textAreaScroller.viewportBoundsProperty().addListener(listener);
+		text.boundsInLocalProperty().addListener(listener);
 
-        ChangeListener<? super Bounds> listener = 
-                (obs, oldBounds, newBounds) -> centerTextIfNecessary(textAreaScroller, text);
-        textAreaScroller.viewportBoundsProperty().addListener(listener);
-        text.boundsInLocalProperty().addListener(listener);
+	}
 
-    }
-
-    private void centerTextIfNecessary(ScrollPane textAreaScroller, Text text) {
-        double textHeight = text.getBoundsInLocal().getHeight();
-        double viewportHeight = textAreaScroller.getViewportBounds().getHeight();
-        double offset = Math.max(0, (viewportHeight - textHeight) / 2 );
-        text.setTranslateY(offset);
-        Parent content = (Parent)textAreaScroller.getContent();
-        for (Node n : content.getChildrenUnmodifiable()) {
-            if (n instanceof Path) { // caret
-                n.setTranslateY(offset);
-            }
-        }
-    }
+	/**
+	 * The centerTextIfNecessary() method is called when each text written. This
+	 * method then calculate the vertical alignment
+	 * 
+	 * @param textAreaScroller
+	 * @param text
+	 */
+	private void centerTextIfNecessary(ScrollPane textAreaScroller, Text text) {
+		double textHeight = text.getBoundsInLocal().getHeight();
+		double viewportHeight = textAreaScroller.getViewportBounds().getHeight();
+		double offset = Math.max(0, (viewportHeight - textHeight) / 2);
+		text.setTranslateY(offset);
+		Parent content = (Parent) textAreaScroller.getContent();
+		for (Node n : content.getChildrenUnmodifiable()) {
+			if (n instanceof Path) { // caret
+				n.setTranslateY(offset);
+			}
+		}
+	}
 }
